79434c0cf1a44ceb25dd2ddc0e56ea02073997b9
Unknown <bkoehler85282@gmail.com>
2019-10-02 15:52:23 -0700

    Setup objects for objects assignment

--- assignments/objects.js
+++ assignments/objects.js
@@ -1,6 +1,6 @@
 // Let's get some practice writing a few objects for a new group of interns at a small business.
 
-// ==== Challenge 1: Writing Objects ==== 
+// ==== Challenge 1: Writing Objects ====
 // HR needs some information on the new interns put into a database.  Given an id, email, first name, and gender. Create an object for each person in the company list:
 
 // 1, mmelloy0@psu.edu, Mitzi, F
@@ -12,15 +12,41 @@
 // Example format of an intern object: 1, examples@you.edu, Example, F
 const example = {
   id: 0,
-  name: "Example",
-  email: "examples@you.edu",
-  gender: "F",
+  name: 'Example',
+  email: 'examples@you.edu',
+  gender: 'F'
 };
 
 // Write your intern objects here:
+const employees = [
+  {
+    id: 0,
+    name: 'Mitzi',
+    email: 'mmelloy0@psu.edu',
+    gender: 'F'
+  },
+  { id: 1, name: 'Kennan', email: 'kdiben1@tinypic.com', gender: 'M' },
+  {
+    id: 2,
+    name: 'Keven',
+    email: 'kmummery2@wikimedia.org',
+    gender: 'M'
+  },
+  {
+    id: 3,
+    name: 'Gannie',
+    email: 'gmartinson3@illinois.edu',
+    gender: 'M'
+  },
+  {
+    id: 4,
+    name: 'Antonietta',
+    email: 'adaine5@samsung.com',
+    gender: 'F'
+  }
+];
 
-
-// ==== Challenge 2: Reading Object Data ==== 
+// ==== Challenge 2: Reading Object Data ====
 // Once your objects are created, log out the following requests from HR into the console:
 
 // Mitzi's name
@@ -33,7 +59,7 @@ const example = {
 
 // Antonietta's Gender
 
-// ==== Challenge 3: Object Methods ==== 
+// ==== Challenge 3: Object Methods ====
 // Give Kennan the ability to say "Hello, my name is Kennan!" Use the console.log provided as a hint.
 // console.log(kennan.speak());
 
@@ -42,7 +68,7 @@ const example = {
 
 // === Great work! === Head over to the the arrays.js. You may come back and attempt the Stretch Challenge once you have completed the challenges in arrays.js and function-conversion.js.
 
-// ==== Stretch Challenge: Nested Objects and the this keyword ==== 
+// ==== Stretch Challenge: Nested Objects and the this keyword ====
 
 // 1. Create a parent object with properties for name and age.  Make the name Susan and the age 70.
 // 2. Nest a child object in the parent object with name and age as well.  The name will be George and the age will be 50.
