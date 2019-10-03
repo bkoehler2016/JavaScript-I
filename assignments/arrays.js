a54305a802b2805cfda7225bb412de47fa244eee
Unknown <bkoehler85282@gmail.com>
2019-10-02 20:31:35 -0700

    fixed spacing

--- assignments/arrays.js
+++ assignments/arrays.js
@@ -327,7 +327,7 @@ let inventory = [{
 function findCarId(id) {
   for (i = 0; i < inventory.length; i++) {
     if (inventory[i].id === id) {
-      return (`Car ` + id + `is a ` + inventory[i].car_year + ` ` + inventory[i].car_make + ` ` + inventory[i].car_model);
+      return (`Car ` + id + ` is a ` + inventory[i].car_year + ` ` + inventory[i].car_make + ` ` + inventory[i].car_model);
     }
   }
 }
